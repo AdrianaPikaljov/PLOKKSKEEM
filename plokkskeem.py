bfa771b8cf25adcaae961879871795d1f70f4454
adri <pikaljovadri@gmail.com>
2025-03-05 23:03:15 +0200

    Update plokkskeem.py

--- plokkskeem.py
+++ plokkskeem.py
@@ -1,3 +1,13 @@
+#4 variant 3 ul
+sobrad = int(input("sisesta soprade arv: "))
+loeb = 0
+
+for i in range(sobrad):
+    vanus = int(input(f"sobra {i + 1} vanus: "))
+    if vanus > 16:
+        loeb += 1
+
+print(f"ohtusoogile paaseb {loeb} sopra.")
 
 #1variant ul 4
 try:
@@ -49,3 +59,42 @@ for o in range(20):
     for e in range(3):
         print(f"{e+1}. eksam")
 
+ #V4 2. Koostage programmi plokkskeem, et arvutada ainult negatiivsete P antud arvude summa.
+vastus=0
+P=int(input("Mitu korda kordame?"))
+while True:
+    arv=float(input("Sisesta arv: "))
+    if arv<0: vastus+=arv
+    P-=1
+    if P==0: break
+print("Summa on: ",vastus)
+
+vastus=0
+P=int(input("Mitu korda kordame?"))
+for i in range(P):
+    arv=float(input("Sisesta arv: "))
+    if arv<0: vastus+=arv
+print("Summa on: ",vastus)
+
+
+#V1 4.(3) Koostage plokkskeem kotlette praadiva roboti jaoks.
+kokku=int(input("Kokku kotlete: "))
+panni_maht=int(input("Panni maht: "))
+aeg=1
+lahenemine=kokku//panni_maht
+jaak=kokku%panni_maht
+if jaak>0: lahenemine+=1
+print(f"Praeme. Tuleb {lahenemine} lahenemist.")
+for l in range(lahenemine):
+    if jaak>0 and l==lahenemine-1:
+        print(f"Panni peal on {jaak} kotlet.")
+    else:
+        print(f"Panni peal on {panni_maht} kotlet.")
+    print(f"{l+1}. lahenemine. Praeme esimene pool.")
+    sleep(aeg)
+    print("Ümberpöörame")
+    print(f"{l+1}. lahenemine. Praeme teine pool.")
+    sleep(aeg)
+    print(f"Valmis!")
+
+print("Kõik kotletid on praetud!")
